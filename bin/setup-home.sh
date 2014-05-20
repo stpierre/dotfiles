b2518fc1c87903cf70583b2eae8307e29e83581b
Chris St. Pierre <chris.a.st.pierre@gmail.com>
2014-05-20 11:32:45 -0400

    actually clone dotfiles into the destination requested

--- bin/setup-home.sh
+++ bin/setup-home.sh
@@ -4,7 +4,7 @@ dest=${DOTFILES_HOME:-$HOME}
 
 clone=$(mktemp -d)
 git clone https://github.com/stpierre/dotfiles.git $clone
-rsync -av $clone/ $HOME/
+rsync -av $clone/ $dest/
 rm -rf $clone
-cd $HOME
+cd $dest
 git status
