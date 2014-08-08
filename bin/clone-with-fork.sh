2f7160707017f195ecb2380968e0f97cdc67ee60
Chris St. Pierre <stpierre@metacloud.com>
2014-08-08 09:24:16 -0400

    made clone-with-fork support non-https repos

--- bin/clone-with-fork.sh
+++ bin/clone-with-fork.sh
@@ -1,12 +1,14 @@
 #!/bin/zsh
 
-upstream=$(echo $1 | sed -n 's#.*github.com/\([^/]*\)/.*#\1#p')
+upstream=$(echo $1 | sed -n 's#.*github.com[:/]\([^/]*\)/.*#\1#p')
+server=$(echo $1 | sed -n 's#\(.*github.com[:/]\).*#\1#p')
 name=$(echo $1 | sed -n 's#.*/\(.*\)\.git#\1#p')
 dest=${2:-$name}
+username=$(git config --get github.user)
 
-git clone https://github.com/$upstream/$name.git $dest
+git clone $server$upstream/$name.git $dest
 cd $dest
 git remote rm origin
-git remote add origin https://github.com/stpierre/$name.git
-git remote add upstream https://github.com/$upstream/$name.git
+git remote add origin $server$username/$name.git
+git remote add upstream $server$upstream/$name.git
 git fetch --all
