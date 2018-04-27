527ad824790cbdbaec07bdab7839be9d71a4803c
Chris St. Pierre <chris.a.st.pierre@gmail.com>
2018-04-27 16:10:26 -0500

    update lxde-monitors.sh for new device naming

--- bin/lxde-monitors.sh
+++ bin/lxde-monitors.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -x
 
 xrandr --output eDP1 --auto \
-       --output DP2 --auto \
-       --output DP1 --auto
+       --output DP1-1 --auto \
+       --output DP1-2 --auto
 
-if xrandr -q | grep -q '^DP1[[:space:]]*connected'; then
-    xrandr --output eDP1 --pos 0x200 \
-           --output DP2 --pos 2880x0 --rotate left \
-           --output DP1 --pos 4080x200
+if xrandr -q | grep -q '^DP1-1[[:space:]]*connected'; then
+    xrandr --output eDP1 --pos 0x600 \
+           --output DP1-1 --pos 1920x0 --rotate left \
+           --output DP1-2 --pos 3120x400
 fi
